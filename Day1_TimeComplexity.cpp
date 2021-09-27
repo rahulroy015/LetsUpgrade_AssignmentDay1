0530f590d2db757219424012a01db635b9ee592a
rahulroy015 <60875599+rahulroy015@users.noreply.github.com>
2021-09-28 01:31:51 +0530

    Add files via upload

---
+++ Day1_TimeComplexity.cpp
@@ -0,0 +1,13 @@
+//given code
+	a=1
+	b=1
+	while(b<n){
+		a += 1
+		b += a
+		cout<<"Hi"
+		
+	}
+	
+// To find complexity
+// so, for this code while loop will iterate b<n times i.e, upto n times ,it will print "Hi".
+// therefor time complexity of this code will O(n)=> T(n)=O(n).
